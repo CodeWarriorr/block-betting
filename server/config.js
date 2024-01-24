d3c6107eb75f3665241b9ceadd7a82f5c8b1b8bb
Michał Mach <vipmachu@gmail.com>
2024-01-24 14:39:15 +0100

    backend config

--- server/config.js
+++ server/config.js
@@ -1,8 +1,10 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
 // Load env vars if env is not production
 if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: './server/config/local.env' })
+  dotenv.config({ path: './server/config/local.env' });
 }
 
 module.exports = {
@@ -11,4 +13,11 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   NODE_ENV: process.env.NODE_ENV,
   INITIAL_CHIPS_AMOUNT: 100000,
-  }
+  RPC_URL: process.env.RPC_URL ?? 'https://polygon-mumbai-bor.publicnode.com',
+  CONTRACT_DEPLOYMENTS_PATH: path.join(
+    __dirname,
+    '..',
+    'deployments',
+    process.env.CHAIN_NAME ?? 'mumbai'
+  ),
+};
